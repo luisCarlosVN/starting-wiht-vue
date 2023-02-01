8bc43360332016c8b5454f09d87f159c6c177320
INSAGS\0007090 <lcsanchez@viewnext.com>
2023-02-01 19:55:28 +0100

    add fist commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App" class="border border-primary"/>
+  <StartingWithVue msg="Bienvenido a tu primer componente" class="border border-success"/><hr>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import StartingWithVue from './components/StartingWithVue.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    StartingWithVue
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/StartingWithVue.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="hello">
+        <div>
+            <h1> {{ msg }}</h1>
+            <p> Hello World from Your Starter Component</p>
+            <br/>
+            <p> 
+                {{ count }} <br/>
+                <button @click="increment">+</button>
+                <button @click="decrement">-</button>
+            </p>
+        </div>
+        <br/>
+        <div>
+            <p> Mensaje original: "{{ msg }}"</p>
+            <p> Mensaje invertido computado: "{{ reverseMsg }}"</p>
+            
+        </div>
+        <br/>
+        <div v-if="Math.random() > 0.5">
+            Ahora me ves
+        </div>
+        <div v-else>
+            Ahora no
+        </div>
+    </div>
+</template>
+
+<script>
+//import { ref } from "vue";
+
+export default
+{
+    name: 'StartingWithVue',
+    props: {
+        msg: {required: true, type: String},
+        type: {default: 'A', type: String}
+    },
+    data: () => ({
+        count: 0
+    }),
+    computed: {
+        reverseMsg: function () {
+            return this.msg.split('').reverse().join('')
+        }
+    }
+
+}
+</script>
